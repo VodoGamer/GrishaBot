34e6f01a0b71ec2306ca0ee4007df8ad9f92a58d
VodoGamer <vodogamer@yandex.ru>
2022-05-11 00:01:00 +0300

    Обзывалка

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,4 +1,5 @@
-from . import rp, models, custom_names, person_of_day, settings, sex, how_long
+from . import (rp, models, custom_names, person_of_day, settings,
+               sex, how_long, insults)
 
 modules_list = [rp.bp, models.bp, custom_names.bp, person_of_day.bp,
-                settings.bp, sex.bp, how_long.bp]
+                settings.bp, sex.bp, how_long.bp, insults.bp]

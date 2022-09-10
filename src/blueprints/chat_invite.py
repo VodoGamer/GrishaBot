9747abe26b61a6286d7cfbfeb9d0d23618bc4edb
VodoGamer <vodogamer@yandex.ru>
2022-09-10 15:32:17 +0300

    update `chat_invite.py`

--- src/blueprints/chat_invite.py
+++ src/blueprints/chat_invite.py
@@ -1,38 +1,49 @@
-from vkbottle_types.objects import MessagesMessageActionStatus
 from vkbottle.bot import Blueprint, Message
 
-from src.db.models import Chat, User
+from src.db.models import Chat, User, ChatCooldown
 
 bp = Blueprint("chat_invite")
+
+
+async def update_chat(chat_id: int):
+    chat_info = (await bp.api.messages.get_conversations_by_id(
+        [chat_id])).items[0].chat_settings
+    if not chat_info:
+        return
+    chat = await Chat.get_or_create(id=chat_id)
+    await ChatCooldown.get_or_create(chat=chat[0])
+
+    chat_members = (await bp.api.messages.get_conversation_members(
+        chat[0].id)).items
+    for user in chat_members:
+        await User.update_or_create(
+            {"is_admin": user.is_admin or False,
+             "is_owner": user.is_owner or False},
+            id=user.member_id, chat=chat[0])
 
 
 @bp.on.chat_message(text=".init")
 async def register_new_chat(message: Message):
     await message.reply("Идёт иницилизация бота")
-    chat_info = (await bp.api.messages.get_conversations_by_id(
-        [message.peer_id])).items[0].chat_settings
-    if not chat_info:
-        return
-
-    chat = await Chat.create(
-        id=message.peer_id,
-        owner_id=chat_info.owner_id)
-
-    chat_members = (await bp.api.messages.get_conversation_members(
-        chat.id)).items
-
-    for user in chat_members:
-        await User.create(id=user.member_id,
-                          chat=chat,
-                          is_admin=user.is_admin or False)
+    await update_chat(message.peer_id)
     await message.reply("Иницилизация завершена!")
 
 
-@bp.on.chat_message()
+@bp.on.chat_message(action="chat_invite_user")
 async def init_new_chat(message: Message):
     if not message.action:
         return
-    if message.action.type == MessagesMessageActionStatus.CHAT_INVITE_USER:
-        await message.answer("Спасибо за приглашение! Для начала работы мне "
-                             "нужны права администратора\nПосле того как вы "
-                             "их предоставите напишите команду: \".init\"")
+    if not message.action.member_id:
+        return
+
+    if message.action.member_id > 0:
+        await update_chat(message.peer_id)
+        return "Привет мешок с костями!"
+    group = (await bp.api.groups.get_by_id())[0]
+    if message.action.member_id != -group.id:
+        await update_chat(message.peer_id)
+        return "Привет брат робот!"
+    return (
+        "Спасибо за приглашение! Для начала работы мне "
+        "нужны права администратора\nПосле того как вы "
+        "их предоставите напишите команду: \".init\"")
